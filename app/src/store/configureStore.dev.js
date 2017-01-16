1313b987326f71d03bcdc18036ec63771864316d
mizuki-y <mizuki-y@syati.info>
2017-01-17 00:59:48 +0900

    Use redux-thunk

--- app/src/store/configureStore.dev.js
+++ app/src/store/configureStore.dev.js
@@ -1,11 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
-
 import DevTools from '../containers/Root/DevTools';
 
-
 const enhancer = compose(
-  applyMiddleware(),
+  applyMiddleware(thunk),
   DevTools.instrument()
 );
 
--- app/src/store/configureStore.prod.js
+++ app/src/store/configureStore.prod.js
@@ -1,8 +1,9 @@
 import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from '../reducers';
 
 const enhancer = compose(
-  applyMiddleware(),
+  applyMiddleware(thunk),
 );
 
 export default function configureStore(initialState) {
