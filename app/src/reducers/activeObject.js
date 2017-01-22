a99057cdc5c278d30c23487b6edbfa87894f090b
mizuki-y <mizuki-y@syati.info>
2017-01-22 16:49:50 +0900

    Use toJSON to get all property

--- app/src/reducers/activeObject.js
+++ app/src/reducers/activeObject.js
@@ -2,14 +2,14 @@ import { handleActions } from 'redux-actions';
 import * as types from '../constants/actionTypes';
 
 export default handleActions({
-  [types.OBJECT_SELECTED]: (state, action) => ({ ...state, ...action.payload }),
-  [types.OBJECT_MOVING]: (state, action) => ({ ...state, ...action.payload }),
-  [types.OBJECT_MODIFIED]: (state, action) => ({ ...state, ...action.payload }),
-  [types.OBJECT_SCALING]: (state, action) => ({ ...state, ...action.payload }),
+  [types.OBJECT_SELECTED]: (state, action) => ({ ...state, ...action.payload.toJSON() }),
+  [types.OBJECT_MOVING]: (state, action) => ({ ...state, ...action.payload.toJSON() }),
+  [types.OBJECT_MODIFIED]: (state, action) => ({ ...state, ...action.payload.toJSON() }),
+  [types.OBJECT_SCALING]: (state, action) => ({ ...state, ...action.payload.toJSON() }),
   [types.OBJECT_CLEARED]: (state, action) => null,
 
-  [types.OBJECT_SET_LEFT]: (state, action) => ({ ...state, ...action.payload }),
-  [types.OBJECT_SET_TOP]: (state, action) => ({ ...state, ...action.payload }),
-  [types.OBJECT_SET_WIDTH]: (state, action) => ({ ...state, ...action.payload }),
-  [types.OBJECT_SET_HEIGHT]: (state, action) => ({ ...state, ...action.payload }),
+  [types.OBJECT_SET_LEFT]: (state, action) => ({ ...state, ...action.payload.toJSON() }),
+  [types.OBJECT_SET_TOP]: (state, action) => ({ ...state, ...action.payload.toJSON() }),
+  [types.OBJECT_SET_WIDTH]: (state, action) => ({ ...state, ...action.payload.toJSON() }),
+  [types.OBJECT_SET_HEIGHT]: (state, action) => ({ ...state, ...action.payload.toJSON() }),
 }, {});
